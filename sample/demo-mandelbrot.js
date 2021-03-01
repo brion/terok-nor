462ef34f9590797c22d00aec561416d819a3a392
Brion Vibber <brion@pobox.com>
2021-02-28 20:34:38 -0800

    Getting good performance now on second or third iteration
    Breakpoints are no longer expensive! They only cost once you get
    into the string of instructions that include them.

--- sample/demo-mandelbrot.js
+++ sample/demo-mandelbrot.js
@@ -11,91 +11,124 @@ const imports = {
 
 (async () => {
 
-    async function test(instance) {
-        var x0 = -2.5, x1 = 1, y0 = -1, y1 = 1;
-        var cols = 72, rows = 24;
-        var maxIters = 1000;
+    // Give some warmup time for the JS compiler!
+    const repeat = 3;
 
-        // Give some warmup time for the function
-        for (let i = 0; i < 1000; i++) {
-            await instance.exports.iterate_mandelbrot(0, 0, maxIters);
-        }
+    async function test(name, setup, after=null) {
+        console.log(`${name}:`);
+        const instance = await setup();
 
-        const start = Date.now();
-        for (var row = 0; row < rows; row++) {
-            var y = (row / rows) * (y1 - y0) + y0;
-            var str = '';
-            for (var col = 0; col < cols; col++) {
-                var x = (col / cols) * (x1 - x0) + x0;
-                var iters = await instance.exports.iterate_mandelbrot(x, y, maxIters);
-                if (iters == 0) {
-                    str += '.';
-                } else if (iters == 1) {
-                    str += '%';
-                } else if (iters == 2) {
-                    str += '@';
-                } else if (iters == maxIters) {
-                    str += ' ';
-                } else {
-                    str += '#';
+        for (let i = 0; i < repeat; i++) {
+            console.log(`(iteration ${i} of ${repeat}:)`);
+
+            var x0 = -2.5, x1 = 1, y0 = -1, y1 = 1;
+            var cols = 72, rows = 24;
+            var maxIters = 1000;
+
+            const start = Date.now();
+            for (var row = 0; row < rows; row++) {
+                var y = (row / rows) * (y1 - y0) + y0;
+                var str = '';
+                for (var col = 0; col < cols; col++) {
+                    var x = (col / cols) * (x1 - x0) + x0;
+                    var iters = await instance.exports.iterate_mandelbrot(x, y, maxIters);
+                    if (iters == 0) {
+                        str += '.';
+                    } else if (iters == 1) {
+                        str += '%';
+                    } else if (iters == 2) {
+                        str += '@';
+                    } else if (iters == maxIters) {
+                        str += ' ';
+                    } else {
+                        str += '#';
+                    }
                 }
+                console.log(str);
             }
-            console.log(str);
+            const delta = Date.now() - start;
+            console.log(delta + ' ms');
         }
-        const delta = Date.now() - start;
-        console.log(delta + ' ms');
+        if (after) {
+            await after(instance);
+        }
     }
 
-    console.log('Native sync execution:');
-    const native = await WebAssembly.instantiate(wasm, imports);
-    await test(native.instance);
-
-    console.log('Interpreted async execution:');
-    const interp = await Interpreter.instantiate(wasm, imports);
-    await test(interp.instance);
-
-    console.log('Debuggable async execution:');
-    const debug = await Interpreter.instantiate(wasm, imports, {
-        debug: true
+    await test('Native sync execution', async () => {
+        const {instance} = await WebAssembly.instantiate(wasm, imports);
+        return instance;
     });
-    await test(debug.instance);
 
-    console.log('Debuggable async execution with an un-hit breakpoint:');
-    let breakpoint = 'fake_location';
+
+    await test('Interpreted async execution', async () => {
+        const {instance} = await Interpreter.instantiate(wasm, imports);
+        return instance;
+    });
+
+
+    async function debug() {
+        const {instance} = await Interpreter.instantiate(wasm, imports, {
+            debug: true
+        });
+        return instance;
+    }
+    await test('Debuggable async execution', async () => {
+        return await debug();
+    });
+
+
     let counted = 0;
-    debug.instance.debugger = async () => {
-        counted++;
-        //const frame = debug.instance.stackTrace(0, 1);
-        //console.log(frame);
-    };
-    debug.instance.setBreakpoint(breakpoint);
-    await test(debug.instance);
-    debug.instance.clearBreakpoint(breakpoint);
-    console.log(`Hit breakpoint ${counted} times!`);
+    let breakpoint = 'fake_location';
+    await test('Debuggable async execution with an un-hit breakpoint', async () => {
+        const instance = await debug();
+        instance.debugger = async () => {
+            counted++;
+            //const frame = instance.stackTrace(0, 1);
+            //console.log(frame);
+        };
+        instance.setBreakpoint(breakpoint);
+        return instance;
+    }, async (instance) => {
+        instance.clearBreakpoint(breakpoint);
+        console.log(`Hit breakpoint ${counted} times!`);
+    });
 
-    console.log('Debuggable async execution with a hit breakpoint:');
-    breakpoint = '5581904';
+
     counted = 0;
-    debug.instance.setBreakpoint(breakpoint);
-    await test(debug.instance);
-    debug.instance.clearBreakpoint(breakpoint);
-    console.log(`Hit breakpoint ${counted} times!`);
+    breakpoint = '5581904';
+    await test('Debuggable async execution with a hit breakpoint', async () => {
+        const instance = await debug();
+        instance.debugger = async () => {
+            counted++;
+            //const frame = instance.stackTrace(0, 1);
+            //console.log(frame);
+        };
+        instance.setBreakpoint(breakpoint);
+        return instance;
+    }, async (instance) => {
+        instance.clearBreakpoint(breakpoint);
+        console.log(`Hit breakpoint ${counted} times!`);
+    });
 
-    console.log('Debuggable async execution with a single-step hook:');
-    debug.instance.debugger = async () => {
-        //const frame = debug.instance.stackTrace(0, 1);
-        //console.log(frame);
-    };
-    debug.instance.singleStep = true;
-    await test(debug.instance);
-    debug.instance.singleStep = false;
+
+    await test('Debuggable async execution with a single-step hook', async () => {
+        const instance = await debug();
+        instance.debugger = async () => {
+            //const frame = instance.stackTrace(0, 1);
+            //console.log(frame);
+        };
+        instance.singleStep = true;
+        return instance;
+    }, async (instance) => {
+        instance.singleStep = false;
+    });
 
     console.log('done.');
 
     const source = path.join(__dirname, 'compiled-mandelbrot.js');
     fs.writeFileSync(
         source,
-        debug.instance.exports.iterate_mandelbrot.toString()
+        (await debug()).exports.iterate_mandelbrot.toString()
     );
     console.log(`wrote source to ${source}`);
 
